c138e6fc706463791ce93e366b97137534698e2b
mougariam-cmyk <mougari.am@gmail.com>
2026-09-17 21:42:05 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,6 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const hostname = request.headers.get('host') || '';
-  const MAIN_DOMAIN = process.env.NEXT_PUBLIC_MAIN_DOMAIN || 'vercel.app';
-  const currentHost = hostname.replace(`.${MAIN_DOMAIN}`, '');
-
-  if (hostname.includes(`.${MAIN_DOMAIN}`) && currentHost !== MAIN_DOMAIN && !hostname.startsWith('www.')) {
-    return NextResponse.rewrite(new URL(`/site/${currentHost}`, request.url));
-  }
-
   return NextResponse.next();
 }
 
